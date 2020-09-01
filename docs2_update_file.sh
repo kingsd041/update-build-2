9e055dbc0f32e4ace4ff7ca32db1ecaecd398d77
xiaolu hong <xiaoluhong@rancher.com>
2020-09-01 17:55:54 +0800

    update token

--- docs2_update_file.sh
+++ docs2_update_file.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-GITHUB_API_TOKEN=${GITHUB_API_TOKEN}
-GITHUB_API_URL=https://api.github.com
-REPO_NAME=cnrancher/docs2
-REPO_BRANCHER=v1.4.x-rancher
-FILE_NAME=update.txt
+export GITHUB_API_TOKEN=${CI_TOKEN}
+export GITHUB_API_URL=https://api.github.com
+export REPO_NAME=cnrancher/docs2
+export REPO_BRANCHER=v1.4.x-rancher
+export FILE_NAME=update.txt
 
-FILE_SHA=$( curl -LSs --location --request GET -H "Authorization: Bearer ${GITHUB_API_TOKEN}" "${GITHUB_API_URL}/repos/${REPO_NAME}/contents/${FILE_NAME}?ref=${REPO_BRANCHER}" | jq -r .sha )
-FILE_CONTENT=$( echo "update $(date +%Y-%m-%d:%H:%M:%S)" | base64 )
+export FILE_SHA=$( curl -LSs --location --request GET -H "Authorization: Bearer ${GITHUB_API_TOKEN}" "${GITHUB_API_URL}/repos/${REPO_NAME}/contents/${FILE_NAME}?ref=${REPO_BRANCHER}" | jq -r .sha )
+export FILE_CONTENT=$( echo "update $(date +%Y-%m-%d_%H:%M:%S)" | base64 )
 
 create_cluster_data()
 {
     cat <<EOF
 {
     "branch": "${REPO_BRANCHER}",
-    "message": "update $(date +%Y-%m-%d:%H:%M:%S)",
+    "message": "update $(date +%Y-%m-%d_%H:%M:%S)",
     "committer": {
         "name": "xiaoluhong",
         "email": "xiaoluhong@rancher.com"
@@ -26,6 +26,6 @@ EOF
 }
 
 curl --location --request PUT \
--H "Authorization: Bearer ${GITHUB_API_TOKEN}" \
--d "$(create_cluster_data)" \
-"${GITHUB_API_URL}/repos/${REPO_NAME}/contents/${FILE_NAME}"
+    -H "Authorization: Bearer ${GITHUB_API_TOKEN}" \
+    -d "$(create_cluster_data)" \
+    "${GITHUB_API_URL}/repos/${REPO_NAME}/contents/${FILE_NAME}"
