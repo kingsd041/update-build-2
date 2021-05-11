99cf34514473f1ed1984d4b5e8826f43392186c4
xiaolu hong <xiaoluhong@rancher.com>
2021-05-11 19:10:57 +0800

    add rke2 images sync

--- cron_sync-rke2-images_dockerhub-to-aliyun.sh
+++ cron_sync-rke2-images_dockerhub-to-aliyun.sh
@@ -38,20 +38,29 @@ docker login ${registry} -u${ALIYUN_ACC} -p${ALIYUN_PW}
 #     fi
 # done
 
-# rke 镜像
-export rke_version=$( curl -L -u $token -s https://api.github.com/repos/rancher/rke/git/refs/tags | jq -r .[].ref | awk -F/ '{print $3}' | grep v | awk -Fv '{print $2}' | grep -v [a-z] | awk -F"." '{arr[$1"."$2]=$3}END{for(var in arr){if(arr[var]==""){print var}else{print var"."arr[var]}}}' | sort -u -t "." -k1nr,1 -k2nr,2 -k3nr,3 )
+## rke 镜像
+#export rke_version=$( curl -L -u $token -s https://api.github.com/repos/rancher/rke/git/refs/tags | jq -r .[].ref | awk -F/ '{print $3}' | grep v | awk -Fv '{print $2}' | grep -v [a-z] | awk -F"." '{arr[$1"."$2]=$3}END{for(var in arr){if(arr[var]==""){print var}else{print var"."arr[var]}}}' | sort -u -t "." -k1nr,1 -k2nr,2 -k3nr,3 )
+#
+#for ver in $( echo "${rke_version}" );
+#do
+#        curl -LSs https://github.com/rancher/rke/releases/download/v${ver}/rke_linux-amd64 -o ./rke${ver}
+#        chmod +x ./rke${ver}
+#        ls -all -h
+#        ./rke${ver} config --system-images --all | grep -v 'time=' >> rancher-images-all.txt
+#done
 
-for ver in $( echo "${rke_version}" );
+# rke2 镜像
+export rke2_version=$( curl -L -u $token -s https://api.github.com/repos/rancher/rke2/git/refs/tags | jq -r .[].ref | awk -F/ '{print $3}' | grep v | awk -Fv '{print $2}' | grep -v -E 'alpha|rc|beta' | sort -r )
+
+for ver in $( echo "${rke2_version}" );
 do
-        curl -LSs https://github.com/rancher/rke/releases/download/v${ver}/rke_linux-amd64 -o ./rke${ver}
-        chmod +x ./rke${ver}
-        ls -all -h
-        ./rke${ver} config --system-images --all | grep -v 'time=' >> rancher-images-all.txt
+        curl -L -u $token -s https://github.com/rancher/rke2/releases/download/v${ver}/rke2-images.linux-amd64.txt -o rke2-images-v${ver}.txt
+        cat rke2-images-v${ver}.txt | grep -v 'time=' >> rancher-images-all.txt
 done
 
 # # k3s 镜像
 # export K3S_VERSION=$( curl -u $token -s https://api.github.com/repos/rancher/k3s/git/refs/tags | jq -r .[].ref | awk -F/ '{print $3}' | grep v | awk -Fv '{print $2}' | grep -v -E "rc|alpha" | sort -u -t "." -k1nr,1 -k2nr,2 -k3nr,3 | grep -v ^0. | grep -v -E '^1.0|^1.10|^1.12|^1.13|^1.14|^1.15|^1.16' )
-# 
+#
 # for K3S in $( echo "${K3S_VERSION}" );
 # do
 #     if [[ -f "k3s-images-v${K3S}.txt" ]] && [[ `cat "k3s-images-v${K3S}.txt" | wc -l` > 3 ]]; then
