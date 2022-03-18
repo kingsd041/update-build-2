e8282d9b5bf33ca78aa2a93fef5a0417c323e56d
xiaolu hong <xiaolu.hong@suse.com>
2022-03-18 14:21:40 +0800

    Add docker login

--- cron_sync-cnrancher-images_dockerhub-to-aliyun.sh
+++ cron_sync-cnrancher-images_dockerhub-to-aliyun.sh
@@ -15,6 +15,7 @@ export token=xiaoluhong:${TOKEN}
 export registry=registry.cn-hangzhou.aliyuncs.com
 
 docker login ${registry} -u${ALIYUN_ACC} -p${ALIYUN_PW}
+docker login -u${DOCKER_HUB_ACC} -p${DOCKER_HUB_PW}
 
 # export CNRANCHER_VERSION=$( curl -s https://api.github.com/repos/rancher/rancher/git/refs/tags | jq -r .[].ref | awk -F/ '{print $3}' | grep v | awk -Fv '{print $2}' | grep -v [a-z] | sort -u -t "." -k1nr,1 -k2nr,2 -k3nr,3 | grep -v ^0. | grep -v ^1. )
 #export CNRANCHER_VERSION=$( curl -L -u $token -s https://api.github.com/repos/cnrancher/pandaria/git/refs/tags | jq -r .[].ref | awk -F/ '{print $3}' | grep -v 'rc' | grep -vE 'v2.2.1-|v2.2.2-|v2.2.3-|v2.2.4-')
