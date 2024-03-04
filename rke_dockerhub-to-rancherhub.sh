19be4d72c94560a772d453559428f4eab774c08b
xiaolu hong <xiaolu.hong@suse.com>
2024-03-04 15:46:31 +0800

    no message

--- rke_dockerhub-to-rancherhub.sh
+++ rke_dockerhub-to-rancherhub.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get install jq -y
 
-sudo bash -c "echo 'nameserver 223.5.5.5' > /etc/resolv.conf"
+#sudo bash -c "echo 'nameserver 223.5.5.5' > /etc/resolv.conf"
 cat /etc/resolv.conf
 
 #ping hub.rancher.cn -w 3
